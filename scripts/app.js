12df4205ccc781cb24bf525f195004c037958bc6
Martin Cross <mncross@gmail.com>
2017-08-20 08:19:38 -0500

    install firebase and add configuration

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyA_VqmyD7kVOzYCR2HFhjuQfBF3Ben4jTo",
+    authDomain: "quiz-20282.firebaseapp.com",
+    databaseURL: "https://quiz-20282.firebaseio.com",
+    projectId: "quiz-20282",
+    storageBucket: "quiz-20282.appspot.com",
+    messagingSenderId: "672862695223"
+};
+firebase.initializeApp(config);
